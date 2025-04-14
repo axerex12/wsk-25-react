c3e8bbcd7c8511067984279e5b2728dd6d897d3d
axerex12 <axel.aaldijk12@gmail.com>
2025-04-14 11:52:46 +0300

    Refactor imports in App component; implement media fetching logic in Home component; add fetchData utility; create test JSON for media data

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,19 @@ import {BrowserRouter, Route, Routes} from 'react-router';
 import './App.css';
 import Home from './views/home';
 import Layout from './components/Layout';
-import Profile from './views/profile';
-import Upload from './views/upload';
-import Single from './views/single';
+import Profile from './views/Profile';
+import Upload from './views/Upload';
+import Single from './views/Single';
 
 function App() {
   return (
     <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/upload" element={<Upload />} />
           <Route path="/single" element={<Single />} />
+          <Route path="/" element={<Home />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/components/SingleView.jsx
+++ src/components/SingleView.jsx
@@ -7,11 +7,7 @@ const SingleView = (props) => {
     setSelectedItem(null)
   }
   return (
-    // TODO: Add JSX for displaying a mediafile here
-    // - use e.g. a <dialog> element for creating a modal
-    // - use item prop to render the media item details
-    // - use img tag for displaying images
-    // - use video tag for displaying videos
+
     <>
       {item && (
         <dialog open>
