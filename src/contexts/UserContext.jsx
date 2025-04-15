712b6777bd02b995ed89f9137f15278c3151f413
axerex12 <axel.aaldijk12@gmail.com>
2025-04-15 11:29:27 +0300

    Refactor authentication logic: streamline handleLogin and handleAutoLogin functions; ensure proper async handling and user state management

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -13,41 +13,36 @@ const UserProvider = ({children}) => {
 
   // login, logout and autologin functions are here instead of components
   const handleLogin = async (credentials) => {
-    try {
-      const loginResult = await postLogin(credentials);
+    const loginResult = await postLogin(credentials);
 
-      localStorage.setItem('token', loginResult.token);
+    localStorage.setItem('token', loginResult.token);
 
-      setUser(loginResult.user);
+    setUser(loginResult.user);
 
-      navigate('/');
-
-    } catch (e) {
-      console.log(e.message);
-    }
+    navigate('/');
   };
 
   const handleLogout = () => {
     try {
-      // TODO: remove token from local storage
       localStorage.removeItem('token');
-      // TODO: set user to null
       setUser(null);
-      
       navigate('/');
     } catch (e) {
       console.log(e.message);
     }
   };
 
-  // handleAutoLogin is used when the app is loaded to check if there is a valid token in local storage
   const handleAutoLogin = async () => {
     try {
-      // TODO: get token from local storage
-      // TODO: if token exists, get user data from API
-      // TODO: set user to state
-      // TODO: navigate to home
+      const token = localStorage.getItem('token');
+      if (token) {
+        const userResponse = await getUserByToken(token);
+        setUser(userResponse.user);
+      }
+      navigate('/');
     } catch (e) {
+      // if token != valid
+      handleLogout();
       console.log(e.message);
     }
   };
